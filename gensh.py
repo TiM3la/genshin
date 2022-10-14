7c870a1a3647422e167e49f227e791af9018403f
TiM3la <87546681+TiM3la@users.noreply.github.com>
2022-10-15 01:59:28 +0300

    почти до конца

--- gensh.py
+++ gensh.py
@@ -12,7 +12,7 @@ def rubl(gri):
 def percent(p, x):
     p = D(p)
     x = D(x)
-    return x * (p / 100)
+    return D(x * (p / 100))
 
 kurs = D(D(1352.78) / D(706.3))
 
@@ -20,4 +20,9 @@ s = rubl(706.30)
 s = D(240)
 for i in range(1, 51):
     if (s + percent(i, s)) * D(0.97) >= s and (s + percent(i, s)) * D(0.97) <= s + percent(17, s):
-        print(s + percent(i, s), i)
+        price = D(s + percent(i, s))
+        print(price, i, '%')
+        print(price + percent(13, price))
+        print('_____', price - s)
+
+print(D(s + percent(D(3.1), s)) * D(0.97))
